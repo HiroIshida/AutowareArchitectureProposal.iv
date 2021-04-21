0fd7210b89297578ff40d31f72f7f535235c438d
HiroIshida <spitfire.docomo@gmail.com>
2021-04-21 19:49:29 +0900

    plot car model

--- planning/scenario_planning/parking/astar_search/python/debug_plot.py
+++ planning/scenario_planning/parking/astar_search/python/debug_plot.py
@@ -2,6 +2,26 @@ import matplotlib.pyplot as plt
 import numpy as np
 import rosbag
 
+class CarModel(object):
+    def __init__(self, length=5.5, width=2.75, base2back=1.5):
+        self.length = length
+        self.width = width
+        self.base2back = base2back
+
+    def _get_four_points(self):
+        back = -1.0 * self.base2back
+        front = self.length - self.base2back
+        right = - 0.5 * self.width
+        left = 0.5 * self.width
+        P = np.array([[back, left], [back, right], [front, right], [front, left]])
+        return P
+
+    def get_four_points(self, pos):
+        pos = np.array(pos)
+        P_ = self._get_four_points()
+        P = P_ + pos[None, :]
+        return P
+
 class Costmap(object):
     def __init__(self, msg):
         info = msg.info
@@ -17,13 +37,24 @@ class Costmap(object):
         self.n_grid = n_grid
         self.origin = origin
 
-    def plot(self, pos_start, pos_goal):
+    def plot(self, pose_start, pose_goal):
         fig, ax = plt.subplots()
         xlin, ylin = [np.linspace(self.b_min[i], self.b_max[i], self.n_grid[i]) for i in range(2)]
         X, Y = np.meshgrid(xlin, ylin)
         ax.contourf(X, Y, self.arr)
-        ax.scatter(pos_start[0], pos_start[1])
-        ax.scatter(pos_goal[0], pos_goal[1])
+
+        car = CarModel()
+
+        def plot_pose(pose):
+            pos_xy = pose[:2]
+            P = car.get_four_points(pos_xy)
+            for idx_pair in [[0, 1], [1, 2], [2, 3], [3, 0]]:
+                i, j = idx_pair
+                ax.plot([P[i, 0], P[j, 0]], [P[i, 1], P[j, 1]], color="red")
+
+
+        plot_pose(pose_start)
+        plot_pose(pose_goal)
         ax.axis("equal")
         plt.show()
 
@@ -31,5 +62,7 @@ bag = rosbag.Bag("../bag/astar.bag")
 for topic, msg, _ in bag.read_messages(topics=["costmap"]):
     costmap_msg = msg
 costmap = Costmap(costmap_msg)
-costmap.plot([3714.06, 73750.3], [3719.11, 73745.8])
+pose_start = [3714.06, 73750.3, 0.0541427, 8.21603e-05, 5.30234e-05, 0.228649, 0.973509]
+pose_goal = [3719.11, 73745.8, 1.00101, 0, 0, 0.853535, 0.521036]
+costmap.plot(pose_start, pose_goal)
 
